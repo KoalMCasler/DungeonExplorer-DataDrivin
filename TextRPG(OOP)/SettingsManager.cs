3c1f179413e101b4ec2cfe007f635cea183f00ef
KoalMCasler <Theeoblivion@gmail.com>
2024-10-24 11:55:16 -0300

    Created Settings Manager

---
+++ TextRPG(OOP)/SettingsManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace TextRPG_OOP_
+{
+    internal class SettingsManager
+    {
+        private Settings baseSettings;
+        private string savedJson;
+        public Settings savedSettings;
+        private string fileName = "Settings.json";
+
+        public SettingsManager() //Constructor
+        {
+            baseSettings = new Settings();
+            string baseToJson = JsonSerializer.Serialize(baseSettings);
+            LoadSettings();
+            if(savedJson != null && savedJson == baseToJson)
+            {
+                savedSettings = JsonSerializer.Deserialize<Settings>(baseToJson);
+            }
+            else if(savedJson != null)
+            {
+                savedSettings = JsonSerializer.Deserialize<Settings>(savedJson);
+            }
+            else
+            {
+                savedSettings = JsonSerializer.Deserialize<Settings>(baseToJson);
+            }
+            string JsonToSave = JsonSerializer.Serialize(savedSettings);
+            File.WriteAllText(fileName, JsonToSave);
+        }
+
+        private void LoadSettings()
+        {
+            if(File.Exists(fileName))
+            {
+                savedJson = File.ReadAllText(fileName);
+            }
+        }
+
+    }
+}
